a164743f5e2d55199ecdb0081c59e3a4438a0627
sanam2711 <sanam2711006@gmail.com>
2026-04-17 11:38:40 +0530

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,22 @@
+def first_diff(s1, s2):
+    n = min(len(s1), len(s2))
+
+    for i in range(n):
+        if s1[i] != s2[i]:
+            return i
+
+    if len(s1) != len(s2):
+        return n
+
+    return -1
+
+
+s1 = input("Enter string1: ")
+s2 = input("Enter string2: ")
+
+result = first_diff(s1, s2)
+
+if result == -1:
+    print("Both are identical")
+else:
+    print("Strings are different at first location:", result)
